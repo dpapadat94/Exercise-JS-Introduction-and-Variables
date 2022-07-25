881ce1278ee61109659c3de1aa7dde8e23db2a18
dpapadat94 <102779032+dpapadat94@users.noreply.github.com>
2022-07-25 13:42:12 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+const firstName = "Dennis";
+
+let lastName;
+
+var age;
+
+// Exercise 2
+lastName = "Papadatos";
+age = 27;
+// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was created in ${createdYear}`);
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectSize = "m";
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
